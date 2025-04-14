4da50a8fc318491dfd1a1ff1922ae39ffd94fd9a
sudipsarker14 <sudipsarker2016@gmail.com>
2025-04-14 15:01:55 +0600

    deshboard added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import IncidentForm from './IncidentForm';
+import ProductsDemo from './Deshboard';
+import './index.css';
+//import './App.css';
+import './flags.css';
 
+/*
 function App() {
   return (
     <div className="App">
@@ -10,5 +15,17 @@ function App() {
     </div>
   );
 }
-
+*/
+function App() {
+  return (
+    <div className='App'>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/incident" element={<IncidentForm />} />
+          <Route path="/deshboard" element={<ProductsDemo />} /> 
+        </Routes>
+    </BrowserRouter>
+    </div>
+  );
+}
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'primeicons/primeicons.css';
+import { PrimeReactProvider } from 'primereact/api';
+import 'primeflex/primeflex.css';
+import 'primereact/resources/primereact.css';
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import './index.css';
+import ProductsDemo from "./Deshboard";
 import App from './App';
+import './flags.css';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
+import IncidentForm from './IncidentForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <PrimeReactProvider>
     <App />
+    </PrimeReactProvider>
+       
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+
+
